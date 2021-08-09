eecd9ad833a88321a9d7350ee2f078e63bd84319
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-08-09 16:41:37 +0200

    Aggiungere i file di progetto.

---
+++ 2) Procedure Ricorsive/2) Procedure Ricorsive.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+
+#include <iostream>
+#include <stdio.h>
+#include <tchar.h>
+#include <sdkddkver.h>
+#include <cstdlib>
+#include <stdlib.h>
+
+int main()
+{
+    cout << "Procedure ricorsive\n";
+    cout << "1) Ricerca intero in array\n\n";
+    cout << "-1) Ricerca intero in array\n\n";
+    cout << ">> ";
+    int scelta;
+    cin >> scelta;
+    switch (scelta)
+    {
+    default:
+        system("CLEAR");
+        return main();
+        break;
+    }
+    return;
+}
+
+void ricerca_in_array() {
+
+}
