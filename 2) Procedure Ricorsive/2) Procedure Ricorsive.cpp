62738697e21bfa3a197fd823917a1ce87591b387
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-08-09 21:16:43 +0200

    Funzione ricerca in array completata

--- 2) Procedure Ricorsive/2) Procedure Ricorsive.cpp
+++ 2) Procedure Ricorsive/2) Procedure Ricorsive.cpp
@@ -1,4 +1,4 @@
-using namespace std;
+﻿using namespace std;
 
 #include <iostream>
 #include <stdio.h>
@@ -6,25 +6,155 @@ using namespace std;
 #include <sdkddkver.h>
 #include <cstdlib>
 #include <stdlib.h>
+#include <string>
+#include <windows.h>
+#pragma execution_character_set( "utf-8" )
+
+void ricerca_in_array(), cambiaDim(), scelta_riempimento(), riempimento_manuale(), riempimento_casuale(), scelta_min_max();
+bool cerca_in_array(int arrNumeri[], int nRicercato, int pos, int dimLogica);
+
+const int dim = 1000000;
+int logic_dim = dim, minVal = 0, maxVal = 500;
+int numeri[dim];
 
 int main()
 {
-    cout << "Procedure ricorsive\n";
-    cout << "1) Ricerca intero in array\n\n";
-    cout << "-1) Ricerca intero in array\n\n";
-    cout << ">> ";
-    int scelta;
-    cin >> scelta;
-    switch (scelta)
-    {
-    default:
-        system("CLEAR");
-        return main();
-        break;
-    }
-    return;
+	SetConsoleOutputCP(65001);	//Imposto il char_set per mostrare i caratteri UTF-8
+	srand(time(NULL));
+	bool finito = false;
+	while (!finito) {
+		cout << "Procedure ricorsive\n";
+		cout << "1) Ricerca intero in array\n";
+		cout << "3) Riempi array di numeri\n";
+		cout << "-1) Exit\n\n";
+		cout << ">> ";
+		int scelta;
+		cin >> scelta;
+		system("CLS");
+		switch (scelta)
+		{
+		case 1:
+			ricerca_in_array();
+			system("PAUSE");
+			break;
+		case 3:
+			scelta_riempimento();
+			break;
+		case -1:
+			finito = true;
+		default:
+			break;
+		}
+		system("CLS");
+	}
+	exit(0);
+	return 0;
 }
 
 void ricerca_in_array() {
+	cout << "Inserire il numero da ricercare\n\n>> ";
+	int ricercato;
+	cin >> ricercato;
+	int pos = 0;
+	cout << ((cerca_in_array(numeri, ricercato, 0, logic_dim)) ? "Il numero inserito è presente nell'array di numeri" : "Il numero inserito non è presente nell'array di numeri") << "\n";
+}
 
+bool cerca_in_array(int *arrNumeri, int nRicercato, int pos, int dimLogica) {
+	if (pos >= dimLogica) return false;
+	else {
+		if (nRicercato == arrNumeri[pos]) return true;
+		else cerca_in_array(arrNumeri, nRicercato, ++pos, dimLogica);
+	}
+}
+
+void cambiaDim() {
+	//Funzione per il cambio di dimensione logica dell'array numeri
+	cout << "Inserire la dimensione dell'array desiderata\n";
+	cout << ">> ";
+	int nuovaDim;
+	cin >> nuovaDim;
+	if (nuovaDim > 0 && nuovaDim <= dim) {
+		logic_dim = nuovaDim;
+		return;
+	}
+	else {
+		system("CLS");
+		cambiaDim();
+	}
+	return;
+}
+
+void scelta_riempimento() {
+	cout << "Desidera riempire l'array\n1) Manualmente\n2) Casualmente\n";
+	cout << ">> \n";
+	int scelta;
+	cin >> scelta;
+	system("CLS");
+	if (scelta == 1) riempimento_manuale();
+	else if (scelta == 2) riempimento_casuale();
+}
+
+void riempimento_manuale() {
+	//Funzione di riempimento del vettore numeri manualmente
+	cout << "Inserire -- per terminare l'inserimento\n";
+	int i = 0;
+	bool finito = false;
+	while (i < dim && !finito) {
+		cout << "N." << i << " >> ";
+		string numero;
+		cin >> numero;
+		try
+		{
+			int numeroInt = stoi(numero);
+			numeri[i] = numeroInt;
+			i++;
+		}
+		catch (const std::exception& exc)
+		{
+			if (numero == "--") finito = true;
+		}
+	}
+	logic_dim = i;
+}
+
+void riempimento_casuale() {
+	//Funzione di riempimento del vettore numeri con valori casuali
+	cambiaDim();
+	scelta_min_max();
+	for (int i = 0; i < logic_dim; i++) numeri[i] = (rand() % (maxVal - minVal)) + minVal;
+}
+
+void scelta_min_max() {
+	bool valido = false;
+	while (!valido) {
+		cout << "Inserire il valore minimo generabile >> ";
+		string min;
+		cin >> min;
+		try
+		{
+			int minimo = stoi(min);
+			minVal = minimo;
+			valido = true;
+		}
+		catch (const std::exception& exc)
+		{
+			system("CLS");
+		}
+	}
+	valido = false;
+	while (!valido) {
+		cout << "Inserire il valore massimo generabile >> ";
+		string max;
+		cin >> max;
+		try
+		{
+			int massimo = stoi(max);
+			maxVal = massimo;
+			valido = true;
+		}
+		catch (const std::exception& exc)
+		{
+			system("CLS");
+		}
+	}
 }
