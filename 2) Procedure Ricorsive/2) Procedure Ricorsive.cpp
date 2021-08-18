0a8154a5574d07debda661a372433731622331a5
dravid3x <39882540+dravid3x@users.noreply.github.com>
2021-08-18 11:37:19 +0200

    Esercizi ricorsivi completati

--- 2) Procedure Ricorsive/2) Procedure Ricorsive.cpp
+++ 2) Procedure Ricorsive/2) Procedure Ricorsive.cpp
@@ -8,15 +8,19 @@
 #include <stdlib.h>
 #include <string>
 #include <windows.h>
+#include <list>
 #pragma execution_character_set( "utf-8" )
 
-void ricerca_in_array(), cambiaDim(), scelta_riempimento(), riempimento_manuale(), riempimento_casuale(), scelta_min_max(), conta_elementi_positivi();
-bool cerca_in_array(int arrNumeri[], int nRicercato, int pos, int dimLogica);
+void ricerca_in_array(), cambiaDim(), scelta_riempimento(), riempimento_manuale(), riempimento_casuale(), scelta_min_max(), scelta_riempimento_lista();
+void conta_elementi_positivi(), riempimento_casuale_lista(), riempimento_manuale_lista(), rimuovi_doppioni_lista(), rimuovi_doppioni(int nRicercato, int offSet), stampaLista();
+bool cerca_in_array(int* arrNumeri, int nRicercato, int pos, int dimLogica);
 int conta_positivi(int* arrNumeri, int pos, int dimLogica, int nPositivi);
 
 const int dim = 1000000;
 int logic_dim = dim, minVal = 0, maxVal = 500;
 int numeri[dim];
+list <int> lista;
+list<int>::iterator i;
 
 int main()
 {
@@ -27,7 +31,10 @@ int main()
 		cout << "Procedure ricorsive\n";
 		cout << "1) Ricerca intero\n";
 		cout << "2) Conta elementi positivi\n";
-		cout << "3) Riempi array di numeri\n";
+		cout << "3) Rimuovi doppioni da lista\n";
+		cout << "4) Riempi array di numeri\n";
+		cout << "5) Riempi lista di numeri\n";
+		cout << "6) Stampa lista\n";
 		cout << "-1) Exit\n\n";
 		cout << ">> ";
 		int scelta;
@@ -44,7 +51,18 @@ int main()
 			system("PAUSE");
 			break;
 		case 3:
+			rimuovi_doppioni_lista();
+			system("PAUSE");
+			break;
+		case 4:
 			scelta_riempimento();
+			break;
+		case 5:
+			scelta_riempimento_lista();
+			break;
+		case 6:
+			stampaLista();
+			system("PAUSE");
 			break;
 		case -1:
 			finito = true;
@@ -115,6 +133,16 @@ void scelta_riempimento() {
 	else if (scelta == 2) riempimento_casuale();
 }
 
+void scelta_riempimento_lista() {
+	cout << "Desidera riempire la lista\n1) Manualmente\n2) Casualmente\n";
+	cout << ">> \n";
+	int scelta;
+	cin >> scelta;
+	system("CLS");
+	if (scelta == 1) riempimento_manuale_lista();
+	else if (scelta == 2) riempimento_casuale_lista();
+}
+
 void riempimento_manuale() {
 	//Funzione di riempimento del vettore numeri manualmente
 	cout << "Inserire -- per terminare l'inserimento\n";
@@ -178,4 +206,70 @@ void scelta_min_max() {
 			system("CLS");
 		}
 	}
+}
+
+void riempimento_casuale_lista() {
+	cambiaDim();
+	scelta_min_max();
+	for (int i = 0; i < logic_dim; i++) lista.push_front((rand() % (maxVal - minVal)) + minVal);
+}
+
+void riempimento_manuale_lista() {
+	//Funzione di riempimento della lista manualmente
+	cout << "Inserire -- per terminare l'inserimento\n";
+	int i = 0;
+	bool finito = false;
+	while (i < dim && !finito) {
+		cout << "N." << i << " >> ";
+		string numero;
+		cin >> numero;
+		try
+		{
+			int numeroInt = stoi(numero);
+			lista.push_front(numeroInt);
+			i++;
+		}
+		catch (const std::exception& exc)
+		{
+			if (numero == "--") finito = true;
+		}
+	}
+	logic_dim = i;
+}
+
+void rimuovi_doppioni_lista() {
+	i = lista.begin();
+	rimuovi_doppioni(*i, 0);
+	cout << "Doppioni rimossi correttamente";
+}
+
+void rimuovi_doppioni(int nRicercato, int offSet) {
+	bool firstFound = false;
+	for (i = lista.begin(); i != lista.end(); i++) {
+		if (*i == nRicercato) {
+			if (!firstFound) {
+				firstFound = true;
+			}
+			else {
+				lista.erase(i);
+				rimuovi_doppioni(nRicercato, offSet);
+				return;
+			}
+		}
+	}
+	offSet++;
+	if (offSet >= lista.size()) return;
+	else {
+		i = lista.begin();
+		advance(i, offSet);
+		rimuovi_doppioni(*i, offSet);
+	}
+	return;
+}
+
+void stampaLista() {
+	for (int numero : lista) {
+		cout << numero << "\n";
+	}
+	cout << "\n";
 }
